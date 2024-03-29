0d25b182a34aeb749d0abb07a77a9c4e41fd4ac5
Wires77 <Wires77@users.noreply.github.com>
2024-03-29 01:41:34 -0500

    Add curl copyright to LICENSE file

--- engine/common/base64.h
+++ engine/common/base64.h
@@ -8,13 +8,13 @@
  *
  * Copyright (C) Daniel Stenberg, <daniel@haxx.se>, et al.
  *
- * This software is licensed as described in the file COPYING, which
+ * This software is licensed as described in the file LICENSE, which
  * you should have received as part of this distribution. The terms
  * are also available at https://curl.se/docs/copyright.html.
  *
  * You may opt to use, copy, modify, merge, publish, distribute and/or sell
  * copies of the Software, and permit persons to whom the Software is
- * furnished to do so, under the terms of the COPYING file.
+ * furnished to do so, under the terms of the LICENSE file.
  *
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
